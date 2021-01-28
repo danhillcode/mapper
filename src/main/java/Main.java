e0438562b1cf696938a540c747b52cff33ed6471
unknown <danhilltravel@hotmail.co.uk>
2021-01-28 14:01:54 +0000

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+//https://www.w3schools.com/java/java_variables.asp
+//https://www.hackerone.com/blog/androidhackingmonth-intro-to-android-hacking
+
+//https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
+// tutorial jdcb https://www.progress.com/blogs/jdbc-tutorial-connecting-to-your-database-using-jdbc
+
+//carto - https://carto.com/developers/carto-js/v3/guides/getting-started/
+
+//https://www.w3schools.com/java/java_variables.asp
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
